8901ea2f3d904650b2b44491faa169221510d91b
Hbalvardi <hbal.2025.p@gmail.com>
2025-10-31 10:20:18 +0330

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,20 +1,29 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
+"""
+URL configuration for school project.
 
-app_name="class"
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('blog_detail/',views.blog_detail,name="blog_detail"),
-    # path('manage-students/',views.manage_students,name="manage-students"),
-    # path('messages/',views.messages,name="messages"),
-    # # path('my-profile/',views.home,name="my-profile"),
-    # path('teacher-action/',views.teacher,name="teacher-action"),
-    # path('',views.home,name="home"),
-    # path('news/',views.news,name="news"),
-    # path('create_class/',views.create_class,name="create_class"),
-    # path('base/',views.base,name="base"),
-    path('',views.home,name='home')
-   
-    
+    path('admin/', admin.site.urls),
+    path('',include('classes.urls',namespace="class")),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
